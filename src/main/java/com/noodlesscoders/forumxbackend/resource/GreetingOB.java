f7e0c94deb4392b0f00ce15672fcc13f6bd3073e
Hennadii Kolesnikov <74913520+geggabyte@users.noreply.github.com>
2022-02-18 11:08:51 +0100

    Initial commit

---
+++ src/main/java/com/noodlesscoders/forumxbackend/resource/GreetingOB.java
@@ -0,0 +1,23 @@
+package com.noodlesscoders.forumxbackend.resource;
+
+public class GreetingOB {
+    private Integer id;
+
+    private String data;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
---
+++ src/test/java/com/noodlesscoders/forumxbackend/ForumxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noodlesscoders.forumxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
