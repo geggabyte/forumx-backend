0cac16abb44475a7e055c5ab2d26781b67e98128
Hennadii Kolesnikov <74913520+geggabyte@users.noreply.github.com>
2022-02-21 21:04:16 +0100

    Mail sender on startup

--- src/main/java/com/noodlesscoders/forumxbackend/ForumxBackendApplication.java
+++ src/main/java/com/noodlesscoders/forumxbackend/ForumxBackendApplication.java
@@ -1,13 +1,34 @@
 package com.noodlesscoders.forumxbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
 public class ForumxBackendApplication {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${privateMail}")
+    private String privateMail;
+
     public static void main(String[] args) {
         SpringApplication.run(ForumxBackendApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendStartedMessage() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(privateMail);
+        message.setSubject("Application status");
+        message.setText("Forumx is up and running");
+        mailSender.send(message);
     }
 
 }
